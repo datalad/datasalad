c9e96b4cd869fe04297e5b7f5d9d81682bf32bfb
Michael Hanke <michael.hanke@gmail.com>
2024-06-06 16:21:54 +0200

    doc(sphinx): basic setup for autosummaries of the API

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,9 @@ release = 'v0.0.1-rc2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autosummary',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
